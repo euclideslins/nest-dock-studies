33188c3580eb31306ba76e0b6333d1dce45e9abf
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-05-06 01:10:14 -0400

    feat: add DTO in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Passwords do not matches');
+    }
+
     const hashed = await bcrypt.hash(body.password, 12);
 
     return this.userService.create({
