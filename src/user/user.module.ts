b8c86daf925b1953b49cfda9ddb69675bdfd5afa
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-05-05 21:21:11 -0400

    feat: add user entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/User';
 import { UserController } from './user.controller';
 
 @Module({
-  controllers: [UserController]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
 })
 export class UserModule {}
