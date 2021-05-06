8b684f0bdf732477aedae2b915174e1b2662f65c
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-05-06 00:45:16 -0400

    feat: add hash for password in authController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
@@ -7,6 +8,13 @@ export class AuthController {
 
   @Post('register')
   async register(@Body() body) {
-    return this.userService.create(body);
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 }
