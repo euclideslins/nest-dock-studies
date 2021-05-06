ea7bc12e6d24ef7babafbfd49692c68253eb51f1
Euclides-Lins <euclidesvasconcelos01@gmail.com>
2021-05-06 01:57:49 -0400

    feat: create login method

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM node:12.19
 WORKDIR /app
 COPY package.json .
 RUN npm install
-
+COPY . .
 
 CMD npm run start:dev
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './models/register.dto';
@@ -21,5 +27,24 @@ export class AuthController {
       email: body.email,
       password: hashed,
     });
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userService.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // eslint-disable-next-line prettier/prettier
+    if (!await bcrypt.compare(password, user.password)) {
+      throw new BadRequestException('Invald credentials');
+    }
+
+    return user;
   }
 }
